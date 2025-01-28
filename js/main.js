e86ca509953ab7acef5bf39206db6146df2378b1
namejone <devnamejone@gmail.com>
2025-01-28 09:13:22 +0600

    Hero compleate

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// mobile-nav
+const mobileNav = document.querySelector('.mnav');
+const closeBtn = document.querySelector('.mnav__close-btn');
+const closeBtnIcon = document.querySelector('.mnav__close-btn-icon');
+
+// Define classes
+const navOpendClass = "left-0";
+const navClosedClass = "-left-[300px]";
+const arrowLeftClass = "ri-arrow-left-s-line";
+const arrowRightClass = "ri-arrow-right-s-line";
+
+// Toggle navigation
+closeBtn.addEventListener('click', () => {
+  if (mobileNav.classList.contains(navClosedClass)) {
+    // Open nav: Add `left-0` and remove `-left-[300px]`
+    mobileNav.classList.remove(navClosedClass);
+    mobileNav.classList.add(navOpendClass);
+
+    // Update icon
+    closeBtnIcon.classList.remove(arrowRightClass);
+    closeBtnIcon.classList.add(arrowLeftClass);
+  } else {
+    // Close nav: Add `-left-[300px]` and remove `left-0`
+    mobileNav.classList.remove(navOpendClass);
+    mobileNav.classList.add(navClosedClass);
+
+    // Update icon
+    closeBtnIcon.classList.remove(arrowLeftClass);
+    closeBtnIcon.classList.add(arrowRightClass);
+  }
+});
