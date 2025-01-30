0b62499b4ef0b6c114dbc6535f3c3ec91937c2b4
namejone <devnamejone@gmail.com>
2025-01-30 23:00:29 +0600

    Faq section added

--- js/main.js
+++ js/main.js
@@ -47,3 +47,15 @@ const swiperTeam = new Swiper('.team__slider', {
     clickable: true,
   },
 });
+
+// faq
+const faqItems = document.querySelectorAll('.faq__item');
+faqItems.forEach((item) => {
+  const faqBtn = item.querySelector('.faq__btn');
+  item.addEventListener('click', () => {
+    const isOpen = item.classList.toggle('open');
+    const iconClass = isOpen ? 'ri-subtract-fill' : 'ri-add-fill';
+    const iconElement = faqBtn.querySelector('i');
+    iconElement.classList = `${iconClass} text-2xl`;
+  });
+});
